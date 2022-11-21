d8867679600b584234d10ada9aba50c1ec1b6b71
MathildeMrz <mathilde.marza2001@gmail.com>
2022-11-21 22:19:43 +0100

    Début interface

--- src/App.java
+++ src/App.java
@@ -1,27 +1,23 @@
-import java.awt.GraphicsDevice;
-import java.awt.GraphicsEnvironment;
+import javafx.scene.paint.Color;
 import java.time.LocalDate;
 import java.util.ArrayList;
 
 import Modele.Courier;
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class App extends Application {
-
+	
+    
 	public static void main(String[] args) {
 		Application.launch(args);
-		
-
 	}
 
 	@Override
@@ -36,6 +32,7 @@ public class App extends Application {
 		Courier c1 = new Courier("Dimitri",12.7);
 		Courier c2 = new Courier("Dominique",12.7);		
 		ListView<Courier> listView = new ListView<Courier>();
+		listView.setOnClickListener(null);
 		listView.getItems().add(c1);
 		listView.getItems().add(c2);
 		
@@ -43,9 +40,7 @@ public class App extends Application {
 		VBox couriers = new VBox(listView);        
 		//VBox vbox2 = new VBox(listView2);        
         HBox hbox = new HBox();        
-        
-        //hbox.getChildren().add(vbox2);
-        
+                
         DatePicker date = new DatePicker();
         
         date.setValue(LocalDate.now());
@@ -58,64 +53,24 @@ public class App extends Application {
         createNewRequest.getChildren().add(new Label("Time-window"));
         
         ComboBox<String> timeWindow = new ComboBox();
-        timeWindow.getItems().add("9h");
-        timeWindow.getItems().add("10h");
-        timeWindow.getItems().add("11h");
+        timeWindow.getItems().add("9h-10h");
+        timeWindow.getItems().add("10h-11h");
+        timeWindow.getItems().add("11h-12h");
         createNewRequest.getChildren().add(timeWindow);
         
         createNewRequest.getChildren().add(new Label("Select a courier:"));
                 
-        //hbox containes two elements
+        //hbox contains two elements
         hbox.getChildren().add(couriers);
-        hbox.getChildren().add(createNewRequest);
-        Scene scene = new Scene(hbox, 300, 120);        
-        stage.setScene(scene);
+        hbox.getChildren().add(createNewRequest);                    
+               
+        Scene scene = new Scene(hbox, 300, 120);
+        stage.setScene(scene);  
         
-        stage.show();       
+        stage.show();
         
-        
-        //Construct a map
-        String[] wkt = {
-        	    "Point (-0.13666168754467312 50.81919869153657743)",
-        	    "Point (-0.13622277073931291 50.82205165077141373)",
-        	    "Point (-0.13545466632993253 50.82512406840893959)",
-        	    "Point (-0.13457683271921211 50.82687973563037787)",
-        	    "Point (-0.13413791591385191 50.82907431965718104)",
-        	    "Point (-0.13951464677951447 50.8294035072611976)",
-        	    "Point (-0.14346489802775639 50.83082998687861931)",
-        	    "Point (-0.14697623247063807 50.83072025767727808)",
-        	    "Point (-0.15004865010815954 50.83390240451614517)",
-        	    "Point (-0.15740050659794308 50.8349996965295432)",
-        	    "Point (-0.16486209228906662 50.83741373895902171)",
-        	    "Point (-0.17276259478555042 50.83894994777778464)",
-        	    "Point (-0.18549118214099652 50.8387304893751022)"
-        	    };
-
-        	    //build line
-        	    /*WKTReader2 reader = new WKTReader2();
-        	    GeometryFactory gf = new GeometryFactory();
-        	    Coordinate[] points = new Coordinate[wkt.length];
-        	    int i=0;
-        	    for(String w:wkt) {
-        	      Point p;
-        	  try {
-        	    p = (Point) reader.read(w);
-        	    points[i++]=p.getCoordinate();
-        	  } catch (ParseException e) {
-        	    // TODO Auto-generated catch block
-        	    e.printStackTrace();
-        	  }
-
-        	    }
-        	    LineString line = gf.createLineString(points);
-        	    SimpleFeatureBuilder builder = new SimpleFeatureBuilder(schema);
-        	    builder.set("locations", line);
-        	    SimpleFeature feature = builder.buildFeature("1");*/
-
-        	    
-        
-    
-
+        //mapView map = new mapView();
+        //map.start(stage);
 		
 	}
 		
---
+++ src/mapView.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.HBox;
+import javafx.scene.shape.Line;
+import javafx.stage.Stage;
+
+public class mapView extends Application{
+
+	public static void main(String[] args) {
+		Application.launch(args);
+	}
+
+	@Override
+	public void start(Stage stage) throws Exception {
+		//Draw map
+		
+		ArrayList<Line> lines = new ArrayList<Line>();
+        Line line1 = new Line(200, 370, 400, 560);
+        Line line2 = new Line(780, 550, 340, 900);
+        Line line3 = new Line(900, 720, 490, 930);
+        Line line4 = new Line(12, 820, 125, 168);  
+
+        lines.add(line1);
+        lines.add(line2);
+        lines.add(line3);
+        lines.add(line4);
+        
+        HBox hbox = new HBox();
+        
+        for (int counter = 0; counter < lines.size(); counter++) { 		      
+        	hbox.getChildren().add(lines.get(counter));
+        }  
+                
+        Scene scene = new Scene(hbox, 300, 120);   
+        stage.setScene(scene);
+        
+        stage.show();
+		
+	}
+
+}
